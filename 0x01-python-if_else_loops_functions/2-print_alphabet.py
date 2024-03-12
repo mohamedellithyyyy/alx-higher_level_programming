4a31f004aae5b2367d1671803c7c3c4f7c81b0f3
Mohamed Ellithy <131733390+mohamedellithyyyy@users.noreply.github.com>
2024-03-12 11:17:27 +0200

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-# Using a single loop and print function with string format
-print(''.join(chr(97 + i) for i in range(26)), end='')
+for i in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(i), end='')
