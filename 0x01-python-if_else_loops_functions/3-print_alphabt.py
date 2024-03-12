7fcad6f5986e3f13754f811aa1b8c667a993bdd4
Mohamed Ellithy <131733390+mohamedellithyyyy@users.noreply.github.com>
2024-03-12 11:19:20 +0200

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-# Using a single loop and print function with string format
-print(''.join(chr(97 + i) for i in range(26) if chr(97 + i) not in ['q', 'e']), end='')
-
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print('{:c}'.format(i), end='')
