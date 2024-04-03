ef5b30d69037dc598eb769c562ecd87a50f7773a
Mohamed Ellithy <131733390+mohamedellithyyyy@users.noreply.github.com>
2024-04-03 16:27:32 +0300

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
-#!/usr/bin/python3
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+# #!/usr/bin/python3
+# for letter in range(97, 123):
+#     print("{}".format(chr(letter)), end="")
+for a in range(ord('a'), ord('z')+1):
+    print("{}".format(chr(a)), end='')
