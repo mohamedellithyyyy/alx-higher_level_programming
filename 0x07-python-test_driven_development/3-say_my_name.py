c2bc8f1ef9f682a5a11560b357984bcf3dc4d817
Mohamed Ellithy <131733390+mohamedellithyyyy@users.noreply.github.com>
2024-08-11 20:56:06 +0300

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """
-Module say_my_name
-Prints a given first name and last name.
+This module is composed by a function prints a message
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints a string with <first_name>
-    and <last_name>.
+    """ Function that prints "My name is <first name> <last name>"
+    Args:
+        first_name: first name
+        last_name: last name
+    Returns:
+        No return
+    Raises:
+        TypeError: If first_name or last_name is not a string
     """
 
     if type(first_name) is not str:
